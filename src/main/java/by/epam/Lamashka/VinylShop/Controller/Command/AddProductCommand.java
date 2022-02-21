5e2b46f94d64fed1ac2d25b7e6725c7d4a4792f0
BoeseKnight <ilia.lamashka@gmail.com>
2022-02-21 14:32:45 +0300

    Some Command Classes were added, without realisation

---
+++ src/main/java/by/epam/Lamashka/VinylShop/Controller/Command/AddProductCommand.java
@@ -0,0 +1,11 @@
+package by.epam.Lamashka.VinylShop.Controller.Command;
+
+import by.epam.Lamashka.VinylShop.View.View;
+import javafx.util.Pair;
+
+public class AddProductCommand implements Command{
+    @Override
+    public Pair<String, View> execute(String parameters) {
+        return null;
+    }
+}
---
+++ src/main/java/by/epam/Lamashka/VinylShop/Controller/Command/AddProductToBasketCommand.java
@@ -0,0 +1,11 @@
+package by.epam.Lamashka.VinylShop.Controller.Command;
+
+import by.epam.Lamashka.VinylShop.View.View;
+import javafx.util.Pair;
+
+public class AddProductToBasketCommand implements Command{
+    @Override
+    public Pair<String, View> execute(String parameters) {
+        return null;
+    }
+}
