9285c8787520ba65cd1c1889b88cb58c9d228e75
BoeseKnight <ilia.lamashka@gmail.com>
2022-02-25 03:10:11 +0300

    factories were added

---
+++ src/main/java/by/epam/Lamashka/VinylShop/dao/DAOFactory.java
@@ -0,0 +1,37 @@
+package by.epam.Lamashka.VinylShop.dao;
+
+import by.epam.Lamashka.VinylShop.entity.Song;
+import by.epam.Lamashka.VinylShop.entity.User;
+import by.epam.Lamashka.VinylShop.entity.VinylProduct;
+import by.epam.Lamashka.VinylShop.entity.VinylRecord;
+import by.epam.Lamashka.VinylShop.service.ServiceFactory;
+import by.epam.Lamashka.VinylShop.service.UserService;
+
+public class DAOFactory {
+    private static final DAOFactory instance = new DAOFactory();
+    private final DAO<User> userDAO = new UserDAO();
+    private final DAO<Song> songDAO = new SongDAO();
+    private final DAO<VinylRecord> vinylRecordDAO = new VinylRecordDAO();
+    private final DAO<VinylProduct> vinylProductDAO = new VinylProductDAO();
+
+    private DAOFactory() {}
+    public static DAOFactory getInstance(){
+        return instance;
+    }
+
+    public DAO<User> getUserDAO() {
+        return userDAO;
+    }
+
+    public DAO<VinylProduct> getVinylProductDAO() {
+        return vinylProductDAO;
+    }
+
+    public DAO<Song> getSongDAO() {
+        return songDAO;
+    }
+
+    public DAO<VinylRecord> getVinylRecordDAO() {
+        return vinylRecordDAO;
+    }
+}
--- src/main/java/by/epam/Lamashka/VinylShop/service/ServiceFactory.java
+++ src/main/java/by/epam/Lamashka/VinylShop/service/ServiceFactory.java
@@ -1,5 +1,15 @@
 package by.epam.Lamashka.VinylShop.service;
 
 public class ServiceFactory {
+  private static final ServiceFactory instance = new ServiceFactory();
+  private final UserService userService = new UserService();
 
+  private ServiceFactory() {}
+  public static ServiceFactory getInstance(){
+    return instance;
+  }
+
+  public UserService getUserService() {
+    return userService;
+  }
 }
